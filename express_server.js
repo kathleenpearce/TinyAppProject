620dc5f21c10642cd8455f8495078034157e925b
Kathleen pearce <kathleenpearce@live.com>
2019-04-09 23:21:02 +0000

    generate random number and things

--- express_server.js
+++ express_server.js
@@ -1,15 +1,37 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); // default port 8080
 
 app.set("view engine", "ejs")
 
+
+
+function generateRandomString() {
+  let randomString = Math.random().toString(36).slice(-6);
+  return randomString;
+}
+while (true) {
+console.log(generateRandomString());
+}
 
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +40,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
