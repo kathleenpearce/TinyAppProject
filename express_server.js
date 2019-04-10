05d1302b589b276007954a2f2e82d0226a5b7fd0
Kathleen pearce <kathleenpearce@live.com>
2019-04-10 22:44:59 +0000

    adding a change from the feature branch

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true})); // default port 8080
+const cookieParser = require('cookie-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser()) // default port 8080
 
 app.set("view engine", "ejs")
 
@@ -16,9 +18,11 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase(shortURL) = req.body.longURL;
+  urlDatabase[shortURL] = req.body.longURL;
   res.redirect('/urls/' + shortURL);
 });
 
@@ -27,25 +31,58 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {
- delete urlDatabase[req.params.shortURL];
- res.redirect("/urls");
-});
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 
 });
 
+app.get("/urls", (req, res) => {
+  console.log(req.cookies)
+  let templateVars = {
+  username: req.cookies["username"],
+  urls: urlDatabase
+ };
+ res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+   shortURL: req.params.shortURL,
+   longURL: urlDatabase[req.params.shortURL],
+   username: req.cookies["username"]
+ };
+ res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+ delete urlDatabase[req.params.shortURL];
+ res.redirect("/urls");
+});
+
+
+
+app.post("urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls/" + req.params.shortURL)
+
+})
+
+app.post("/login", (req, res) =>{
+ res.cookie('username', req.body.username);
+ res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username);
+  res.redirect("/urls");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// getting one url
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -55,7 +92,3 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
